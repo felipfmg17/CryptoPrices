d4b091eff38c6e59746018c0876ac95688b71a05
ssdespostrade <ssdespostrade@grupobmv.com.mx>
2017-12-26 18:12:22 -0600

    pipenv installed for managing packages, database script created, and mysql packagae and connections test files created

---
+++ script_db/db_creation.sql
@@ -0,0 +1,35 @@
+
+
+
+CREATE TABLE exchange (
+	id 		SMALLINT 	NOT NULL AUTO_INCREMENT,
+	name 	CHAR(35) 	NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE currency_pair (
+	id 		SMALLINT 	NOT NULL AUTO_INCREMENT,
+	name 	CHAR(35)	NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE price_type (
+	id 		SMALLINT	NOT NULL AUTO_INCREMENT,
+	name 	CHAR(30)	NOT NULL,
+	PRIMARY KEY (id) 	
+);
+
+
+
+CREATE TABLE  coin_price (
+	date_time_sec 		BIGINT 		NOT NULL,
+	exchange_id 		SMALLINT 	NOT NULL,
+	currency_pair_id 	SMALLINT 	NOT NULL,
+	price 				DOUBLE		NOT NULL,
+	date_time 			DATETIME  	NOT NULL,
+	price_type_id			SMALLINT 	NOT NULL,
+	PRIMARY KEY (date_time_sec, exchange_id, currency_pair_id ),
+	FOREIGN KEY (exchange_id)  REFERENCES exchange (id),
+	FOREIGN KEY (currency_pair_id) REFERENCES currency_pair (id),
+	FOREIGN KEY (price_type_id) REFERENCES price_type (id)
+);
